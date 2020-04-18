35ce2f11819dd9b7c05c4d353a62fc32be5e02dc
Muhammad Tirmazi <tirmazi@g.harvard.edu>
2020-04-18 17:03:51 -0400

    Added review for Jessica

--- arrival.c
+++ arrival.c
@@ -1,27 +1,31 @@
 
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-#include <time.h>
-#include <sys/time.h>
-#include <math.h>
 #include <limits.h>
+#include <math.h>
 #include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/time.h>
+#include <time.h>
 
+// R5: the local header should always be the first one included to avoid hiding
+// dependencies
 #include "arrival.h"
 
-poissonProcess init_poissonProcess (double seed, double lambda){
-	poissonProcess p={.seed = seed, .lambda=lambda};
-	srand(p.seed);
-	return p;
+// R6: let's ensure srand gets changed at a higher level e.g at one place at the
+// start of the simulation. Don't worry about it in individual functions. (You
+// can remove the parameter "seed" and line 2 here, we really just need lambda)
+poissonProcess init_poissonProcess(double seed, double lambda) {
+  poissonProcess p = {.seed = seed, .lambda = lambda};
+  srand(p.seed);
+  return p;
 }
 
-// The interevent times in a Poisson process form an iid sequence of exponential RV with mean 1/λ.
-double get_arrival_time(poissonProcess* p){
-	double u = (double) rand() / (double)(RAND_MAX); //uniform from [0,1]
-	double mean = 1 / p->lambda; //mean of the exponential dist
-	double arrival_time = -log(u)*mean; //next arrival
-	return arrival_time;
+// The interevent times in a Poisson process form an iid sequence of exponential
+// RV with mean 1/λ.
+double get_arrival_time(poissonProcess *p) {
+  double u = (double)rand() / (double)(RAND_MAX); // uniform from [0,1]
+  double mean = 1 / p->lambda;          // mean of the exponential dist
+  double arrival_time = -log(u) * mean; // next arrival
+  return arrival_time;
 }
-
-
--- arrival.h
+++ arrival.h
@@ -1,18 +1,25 @@
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-#include <time.h>
-#include <sys/time.h>
-#include <math.h>
-#include <limits.h>
-#include <stdint.h>
+// R1: add header guards
 
+#include <limits.h>
+#include <math.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/time.h>
+#include <time.h>
+
+// R2: Avoid typedeffing structs to remain consistent with the C standard
+// library.
 typedef struct {
-	double seed;
-	double lambda;
+  double seed;
+  double lambda;
+  // R4: rename to poisson_process_t
 } poissonProcess;
 
-poissonProcess init_poissonProcess (double seed, double lambda);
+// R3: Rename to init_poisson_process to be consistent with the rest of our API.
+poissonProcess init_poissonProcess(double seed, double lambda);
 
-// The interevent times in a Poisson process form an iid sequence of exponential RV with mean 1/λ.
-double get_arrival_time(poissonProcess* p);
+// The interevent times in a Poisson process form an iid sequence of exponential
+// RV with mean 1/λ.
+double get_arrival_time(poissonProcess *p);
