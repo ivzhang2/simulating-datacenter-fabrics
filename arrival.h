b71d885e569fb129e2b99646530f242b022a8e97
Jessica Wijaya <52982383+wijayajessica@users.noreply.github.com>
2020-04-18 15:11:13 -0700

    update arrival.h
    -remove poisson process object since we no longer take in seed as inputs here (per comment R7)
    -remove int_poissonProcess
    -change get_arrival_time to take in a single parameter lambda

--- arrival.h
+++ arrival.h
@@ -9,17 +9,6 @@
 #include <sys/time.h>
 #include <time.h>
 
-// R2: Avoid typedeffing structs to remain consistent with the C standard
-// library.
-typedef struct {
-  double seed;
-  double lambda;
-  // R4: rename to poisson_process_t
-} poissonProcess;
-
-// R3: Rename to init_poisson_process to be consistent with the rest of our API.
-poissonProcess init_poissonProcess(double seed, double lambda);
-
 // The interevent times in a Poisson process form an iid sequence of exponential
 // RV with mean 1/λ.
-double get_arrival_time(poissonProcess *p);
+double get_arrival_time(double lambda);
