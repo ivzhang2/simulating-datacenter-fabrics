c9a483d7e679506f4ded6623ecf66501edf1355d
Jessica Wijaya <52982383+wijayajessica@users.noreply.github.com>
2020-04-23 15:04:46 -0700

    update simulation.c to reflect the changes at arrival.c
    update:
    - set the seed from the top of the simulation to be used for all functions downstreeam (not attaching it poissonprocess arrival time only)
    - no longer need to initialize a poissonprocess object, we can pass in the lambda and call the function get_arrival_time directly (since poissonproces is memoryless, so we don't have to keep tract of the state of the poissonprocess object hence we remove the struct completely)

--- simulation.c
+++ simulation.c
@@ -14,15 +14,20 @@ void simulation(char* filename, double interarrival_time, double total_simulatio
 
 	double seed = rand();
 	double lambda = total_simulation_time/interarrival_time;
-
-	poissonProcess arrival_times = init_poissonProcess(seed, lambda);
+	
+	// set the seed from the top of the simulation to be used for all functions downstreeam (not attaching it poissonprocess arrival time only)
+	srand(seed); 
+	
 	struct FlowNode* data_flows = init_flow(filename);
 
 	double next_arrival;
 	double next_data_flow;
 
 	while(total_simulation_time > 0){
-		next_arrival = get_arrival_time(&arrival_times);
+		// no longer need to initialize a poissonprocess object, 
+		// we can pass in the lambda and call the function get_arrival_time directly
+		next_arrival = get_arrival_time(lambda); 
+		
 		next_data_flow = get_next_flow(data_flows);
 		total_simulation_time -= next_arrival;
 		fprintf(fp,"%lf,%lf \n", next_arrival, next_data_flow); // write to csv in format "arrival time, data size \n"
