665a9f63a274ce091768edac4d03bfc3e63a220d
Muhammad Tirmazi <tirmazi@g.harvard.edu>
2020-04-24 15:25:45 -0400

    Made Makefile more robust

--- Makefile
+++ Makefile
@@ -1,17 +1,49 @@
-all: simulation.o flow.o arrival.o
-	gcc simulation.o flow.o arrival.o -o simulation
+VPATH=src/:include/
 
-flow.o: flow.c flow.h
-	gcc -c flow.c
+CC=gcc
+INCLUDE=include/
+CFLAGS=-I $(INCLUDE) -Wall -Werror
+LIB=-lm
+CC_CMD=$(CC) $(CFLAGS)
 
-arrival.o: arrival.c arrival.h
-	gcc -c arrival.c
+# Note: apparently, linked library files should go *after* source files
 
-simulation.o: simulation.c arrival.h flow.h
-	gcc -c simulation.c
+OBJ_DIR=obj
+BIN_DIR=bin
+DEP_DIR=deps
+
+SRCS=$(wildcard *.c)
+DEPS=$(srcs:.c=.d)
+
+all: dir_tgt simulation
+
+simulation: flow.o arrival.o simulation.o
+	$(CC_CMD) $(^:%.o=$(OBJ_DIR)/%.o) -o $(BIN_DIR)/$@ $(LIB)
 
 run: simulation
-	./simulation
+	$(BIN_DIR)/simulation
 
-clean: 
-	rm simulation *.o *.gch
+.PHONY: clean obj_dir_tgt bin_dir_tgt dep_dir_tgt
+
+%.o : %.c
+	$(CC) $(CFLAGS) -MMD -MP -c $< -o $(OBJ_DIR)/$@
+
+MKDIR_P=mkdir -p
+
+dir_tgt: obj_dir_tgt bin_dir_tgt dep_dir_tgt
+
+obj_dir_tgt:
+	$(MKDIR_P) $(OBJ_DIR)
+
+bin_dir_tgt:
+	$(MKDIR_P) $(BIN_DIR)
+
+dep_dir_tgt:
+	$(MKDIR_P) $(DEP_DIR)
+
+clean:
+	rm -rf $(BIN_DIR)
+	rm -rf $(OBJ_DIR)
+	rm -rf $(DEP_DIR)
+
+-include $(DEPS)
--- src/simulation.c
+++ src/simulation.c
@@ -1,45 +1,47 @@
-#include <time.h>
 #include <stdlib.h>
+#include <time.h>
 
-#include "flow.h"
 #include "arrival.h"
+#include "flow.h"
 
-/* 
-	Given: filename of the CDF, interarrival time between flows, and the total simulation time (milliseconds)
-	Returns large csv ("simulation.csv") with interarrival times
+/*
+        Given: filename of the CDF, interarrival time between flows, and the
+   total simulation time (milliseconds) Returns large csv ("simulation.csv")
+   with interarrival times
 */
-void simulation(char* filename, double interarrival_time, double total_simulation_time){
-	FILE *fp;
-	fp=fopen("simulation.csv","w+");
+void simulation(char *filename, double interarrival_time,
+                double total_simulation_time) {
+  FILE *fp;
+  fp = fopen("results/simulation.csv", "w+");
 
-	double seed = rand();
-	double lambda = total_simulation_time/interarrival_time;
-	
-	// set the seed from the top of the simulation to be used for all functions downstreeam (not attaching it poissonprocess arrival time only)
-	srand(seed); 
-	
-	struct FlowNode* data_flows = init_flow(filename);
+  double seed = rand();
+  double lambda = total_simulation_time / interarrival_time;
 
-	double next_arrival;
-	double next_data_flow;
+  // set the seed from the top of the simulation to be used for all functions
+  // downstreeam (not attaching it poissonprocess arrival time only)
+  srand(seed);
 
-	while(total_simulation_time > 0){
-		// no longer need to initialize a poissonprocess object, 
-		// we can pass in the lambda and call the function get_arrival_time directly
-		next_arrival = get_arrival_time(lambda); 
-		
-		next_data_flow = get_next_flow(data_flows);
-		total_simulation_time -= next_arrival;
-		fprintf(fp,"%lf,%lf \n", next_arrival, next_data_flow); // write to csv in format "arrival time, data size \n"
-	}
+  struct flow_node_t *data_flows = init_flow(filename);
 
-	fclose(fp);
+  double next_arrival;
+  double next_data_flow;
 
+  while (total_simulation_time > 0) {
+    // no longer need to initialize a poissonprocess object,
+    // we can pass in the lambda and call the function get_arrival_time directly
+    next_arrival = get_arrival_time(lambda);
 
+    next_data_flow = get_next_flow(data_flows);
+    total_simulation_time -= next_arrival;
+    fprintf(
+        fp, "%lf,%lf \n", next_arrival,
+        next_data_flow); // write to csv in format "arrival time, data size \n"
+  }
+
+  fclose(fp);
 }
 
-
-int main(){
-	simulation("test_cdf.txt", 5.0, 100.0);
-	return 0;
+int main() {
+  simulation("tests/test_cdf.txt", 5.0, 100.0);
+  return 0;
 }
