f6594ea750929758fbcecc92704e775503f4fdc2
Jessica A Wijaya <jessica@Jessicas-MacBook-Pro-2.local>
2020-04-25 13:23:13 -0700

    adding service.c to get the service time

---
+++ include/service.h
@@ -0,0 +1,7 @@
+#include <time.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+
+// get the delay time given lambda (average time between processing)
+double get_service_delay(double lambda);
