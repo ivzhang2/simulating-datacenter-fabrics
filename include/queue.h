8f2c29240ccff97c1e6936a8132b6d7ee32cecd3
ejohnsson <ejohnsson@college.harvard.edu>
2020-04-27 21:17:49 -0400

    clean queue

--- include/queue.h
+++ include/queue.h
@@ -13,25 +13,14 @@ struct packet
 
 struct queue_t
 {
-    int size_t;
-    struct packet* front;
-    struct packet* rear;
+    long last_dequeue_time;
 };
 
 // Initialize a queue
 void queue_initialize(struct queue_t* q);
 
-// Display the queue
-void queue_display(struct packet* head);
-
-// Is the queue empty
-int queue_isempty(struct queue_t* q);
-
-// Returns the time at which the enqueued flow will leave the switch and enqueues the flow
-long queue_enqueue(struct queue_t* q, long start_time, long wait_time);
-
-// Dequeues the next flow and returns its wait time
-int queue_dequeue(struct queue_t* q);
+// Returns the time at which the enqueued flow will leave the switch and enqueues the packet
+long queue_enqueue(struct queue_t* q, struct packet* p);
 
 // Free entire queue
 void queue_free(struct queue_t *q);
--- src/queue.c
+++ src/queue.c
@@ -6,80 +6,22 @@
 
 void initialize(struct queue_t* q)
 {
-    q->size_t = 0;
-    q->front = NULL;
-    q->rear = NULL;
+    q->last_dequeue_time = 0;
 }
 
-void packet_display(struct packet* head)
+// Returns the time at which the enqueued flow will leave the switch and enqueues the packet
+long queue_enqueue(struct queue_t* q, struct packet* p)
 {
-    if(head == NULL)
-    {
-        printf("NULL\n");
+    if (p->start_time >= q->last_dequeue_time){
+        q->last_dequeue_time = p->start_time + p->wait_time;
     }
-    else
-    {
-        printf("%d\n", head -> wait_time);
-        packet_display(head->next);
+    else {
+        q->last_dequeue_time += p->wait_time;
     }
+    return q->last_dequeue_time;
 }
 
-int isempty(struct queue_t* q)
-{
-    return (q->rear == NULL);
-}
-
-// Returns the time at which the enqueued flow will leave the switch and enqueues the flow
-long queue_enqueue(struct queue_t* q, long start_time, long wait_time)
-{
-        struct packet* tmp;
-        tmp = malloc(sizeof(struct packet));
-        assert(tmp != NULL);
-        tmp->start_time = start_time;
-        tmp->wait_time = wait_time;
-        tmp->next = NULL;
-        q->size_t++;
-        if(!isempty(q))
-        {
-            q->rear->next = tmp;
-            q->rear = tmp;
-            long dequeue_time = q->front->start_time;
-            struct packet* cur_packet = q->front;
-            while (cur_packet != NULL)
-            {
-                dequeue_time += cur_packet->wait_time;
-                cur_packet = cur_packet->next;
-            }
-            return dequeue_time;
-        }
-        else // First packet in queue
-        {
-            q->front = q->rear = tmp;
-            return start_time + wait_time;
-        }
-}
-
-// Returns wait time of dequeued flow
-int queue_dequeue(struct queue_t* q)
-{
-    struct packet* tmp;
-    long n = q->front->wait_time;
-    tmp = q->front;
-    q->front = q->front->next;
-    q->size_t--;
-    free(tmp);
-    return(n);
-}
-
-// Free entire queue
 void queue_free(struct queue_t* q)
 {
-    struct packet* n = q->front;
-    while (n != NULL) {
-        struct packet *victim = n;
-        n = n->next;
-        free (victim);
-    }
-    if (n) free (n);
     if (q) free (q);
 }
